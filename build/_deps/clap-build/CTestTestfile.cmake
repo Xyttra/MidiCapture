53f4a82ad29597c5f04caff279795ef65196b67e
Xyttra <32438379+Xyttra@users.noreply.github.com>
2026-03-26 23:23:00 +0300

    Delete build directory

--- build/_deps/clap-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Yusuf/Documents/Antigravity Workspaces/MidiCapture/build/_deps/clap-src
-# Build directory: C:/Users/Yusuf/Documents/Antigravity Workspaces/MidiCapture/build/_deps/clap-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
